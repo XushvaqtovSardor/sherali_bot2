e0454f094c6ebd8022f8657e1d49df0e9b14ac72
fn25 <xushvaqtovsardor032@gmail.com>
2026-01-10 12:48:11 +0500

    fianl 2

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4010;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
